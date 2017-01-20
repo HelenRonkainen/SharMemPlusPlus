4fed65acd21328e55e6b530fbd2b99da878f87cc
Boris Popov <popov@whitekefir.ru>
2017-01-20 13:55:21 +0300

    add dumb files for Mode

---
+++ src/Mode.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#ifndef __linux_posix_Mode__
+#define __linux_posix_Mode__
+
+namespace linux {
+     namespace posix {
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+     }
+}
+#endif // __linux_posix_Mode__
