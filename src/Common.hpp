93b884e45dfd0bad3c8a2e02197b90768d730e50
Boris Popov <popov@whitekefir.ru>
2017-01-20 13:53:06 +0300

    add Name

---
+++ src/Common.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include <string>
+
+#ifndef __linux_posix_Common__
+#define __linux_posix_Common__
+
+namespace linux {
+     namespace posix {
+//////////////////////////////////////////////////////////////////
+	  using String = std::string;
+//////////////////////////////////////////////////////////////////
+     }
+}
+#endif // __linux_posix_Common__
---
+++ src/Name.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include "Common.hpp"
+
+#ifndef ___linux_posix_Name__
+#define ___linux_posix_Name__
+
+namespace linux {
+     namespace posix {
+//////////////////////////////////////////////////////////////////
+	  using Name = String;
+//////////////////////////////////////////////////////////////////
+     }
+}
+#endif // __linux_posix_Name__
