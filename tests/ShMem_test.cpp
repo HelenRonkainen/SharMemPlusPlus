3f757f1c8871a0d472fb8bfd50cbdbdc6954e638
Boris Popov <popov@whitekefir.ru>
2017-01-23 16:16:44 +0300

    add some unit tests for OpenOptions

--- tests/ShMem_test.cpp
+++ tests/ShMem_test.cpp
@@ -6,6 +6,57 @@ TEST(ShMem, test_001)
 {
      using namespace linux;
      posix::Name n("/ShMem_test");
-     //ASSERT_TRUE(true);
+     posix::OpenOptions f;
+     ASSERT_EQ(00644, f.get_mode().get());
+}
+
+TEST(ShMem, test_002)
+{
+     using namespace linux;
+     posix::Name n("/ShMem_test");
+     posix::OpenOptions f;
+     ASSERT_EQ(O_CREAT | O_RDWR, f.get_oflag().get());
+}
+
+TEST(ShMem, test_003)
+{
+     using namespace linux;
+     posix::Name n("/ShMem_test");
+     posix::Mode m;
+     posix::OpenOptions f(m);
+     ASSERT_EQ(O_CREAT | O_RDWR, f.get_oflag().get());
+     ASSERT_EQ(00644, f.get_mode().get());
+}
+
+TEST(ShMem, test_004)
+{
+     using namespace linux;
+     posix::Name n("/ShMem_test");
+     posix::OFlag m;
+     posix::OpenOptions f(m);
+     ASSERT_EQ(O_CREAT | O_RDWR, f.get_oflag().get());
+     ASSERT_EQ(00644, f.get_mode().get());
+}
+
+TEST(ShMem, test_005)
+{
+     using namespace linux;
+     posix::Name n("/ShMem_test");
+     posix::OFlag m1;
+     posix::Mode m2;
+     posix::OpenOptions f(m1, m2);
+     ASSERT_EQ(O_CREAT | O_RDWR, f.get_oflag().get());
+     ASSERT_EQ(00644, f.get_mode().get());
+}
+
+TEST(ShMem, test_006)
+{
+     using namespace linux;
+     posix::Name n("/ShMem_test");
+     posix::OFlag m1;
+     posix::Mode m2;
+     posix::OpenOptions f(m2, m1);
+     ASSERT_EQ(O_CREAT | O_RDWR, f.get_oflag().get());
+     ASSERT_EQ(00644, f.get_mode().get());
 }
 //////////////////////////////////////////////////////////////////
