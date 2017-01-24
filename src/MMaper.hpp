52a5a21d24523f00aaf266e6fc8d4adfff29c471
Boris Popov <popov@whitekefir.ru>
2017-01-24 16:08:07 +0300

    begin MMaper

---
+++ src/MMaper.hpp
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2017, Boris Popov <popov@whitekefir.ru>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include "MMapOptions.hpp"
+
+#ifndef __linux_posix_MMaper__
+#define __linux_posix_MMaper__
+
+namespace linux {
+     namespace posix {
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+     }
+}
+#endif // __linux_posix_MMaper__
