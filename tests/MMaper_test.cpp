afd7caf64be93e05962072df59d7c8b42338b5b8
Boris Popov <popov@whitekefir.ru>
2017-01-25 13:18:01 +0300

    add unit test for MMaper

--- tests/MMaper_test.cpp
+++ tests/MMaper_test.cpp
@@ -24,9 +24,20 @@ TEST(MMaper, test_002)
      posix::SharedMemory sm3(n, false);
      sm3.open().truncate(4096);
      posix::MMapOptions mo;
-     // ASSERT_EQ(posix::mmap::READ |
-     // 	       posix::mmap::WRITE,
-     // 	       mo.get_protection().get());
-     // ASSERT_EQ(posix::mmap::MSHARED, mo.get_flag().get());
+     posix::MMaper m1(sm3, true, mo);
+     posix::MMaper m2(sm3, true);
+     posix::MMaper m3(sm3);
+     m3.unmap();
+}
+
+TEST(MMaper, test_003)
+{
+     using namespace linux;
+     posix::Name n("/ARTA");
+     posix::SharedMemory sm3(n, false);
+     sm3.open().truncate(4096);
+     posix::MMaper m3(sm3);
+     m3.map();
+     m3.unmap();
 }
 //////////////////////////////////////////////////////////////////
