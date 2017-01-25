5e5dc2b5bc2c01d14f000aded9ce6e37c389c4e3
Boris Popov <popov@whitekefir.ru>
2017-01-25 16:48:52 +0300

    add unit test for Semaphore

--- tests/Semaphore_test.cpp
+++ tests/Semaphore_test.cpp
@@ -8,11 +8,22 @@ TEST(Semaphore, test_01)
      ASSERT_TRUE(true);
      Name n("/ARTA");
      OpenOptions f;
-     Semaphore s(n, false, f, 4);
-     
-     // posix::SharedMemory sm3(n, false, f);
-     // sm3.open().truncate(4096);
-     // posix::MMapOptions mo;
+     Semaphore s1(n, false, f, 4);
+     Semaphore s2(n, false);
+     // ASSERT_EQ(posix::mmap::READ |
+     // 	       posix::mmap::WRITE,
+     // 	       mo.get_protection().get());
+     // ASSERT_EQ(posix::mmap::MSHARED, mo.get_flag().get());
+}
+
+TEST(Semaphore, test_02)
+{
+     using namespace linux::posix;
+     ASSERT_TRUE(true);
+     Name n("/AA");
+     Semaphore s2(n, true);
+     s2.open();
+     s2.close();
      // ASSERT_EQ(posix::mmap::READ |
      // 	       posix::mmap::WRITE,
      // 	       mo.get_protection().get());
