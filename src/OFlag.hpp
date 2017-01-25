1255d2bceb950fc0ecda58ddabe950e08c0c6d04
Boris Popov <popov@whitekefir.ru>
2017-01-25 13:56:36 +0300

    add some linux::posix::oflags

--- src/OFlag.hpp
+++ src/OFlag.hpp
@@ -16,11 +16,27 @@ namespace linux {
      namespace posix {
 	  namespace oflag {
 //////////////////////////////////////////////////////////////////
-	       const flag_t CREAT  = O_CREAT;
-	       const flag_t RDWR   = O_RDWR;
-	       const flag_t RDONLY = O_RDONLY;
-	       const flag_t EXCL   = O_EXCL;
-	       const flag_t TRUNC  = O_TRUNC;
+	       const flag_t CREAT     = O_CREAT;
+	       const flag_t RDWR      = O_RDWR;
+	       const flag_t RDONLY    = O_RDONLY;
+	       const flag_t EXCL      = O_EXCL;
+	       const flag_t TRUNC     = O_TRUNC;
+	       const flag_t WRONLY    = O_WRONLY;
+	       const flag_t APPEND    = O_APPEND;
+	       const flag_t ASYNC     = O_ASYNC;
+	       const flag_t CLOEXEC   = O_CLOEXEC;
+	       const flag_t DIRECT    = O_DIRECT;
+	       const flag_t DIRECTORY = O_DIRECTORY;
+	       const flag_t DSYNC     = O_DSYNC;
+	       const flag_t LARGEFILE = O_LARGEFILE;
+	       const flag_t NOATIME   = O_NOATIME;
+	       const flag_t NOCTTY    = O_NOCTTY;
+	       const flag_t NOFOLLOW  = O_NOFOLLOW;
+	       const flag_t NONBLOCK  = O_NONBLOCK;
+	       const flag_t NDELAY    = O_NDELAY;
+	       const flag_t PATH      = O_PATH;
+	       const flag_t SYNC      = O_SYNC;
+	       const flag_t TMPFILE   = O_TMPFILE;
 //////////////////////////////////////////////////////////////////
 	  }
      }
